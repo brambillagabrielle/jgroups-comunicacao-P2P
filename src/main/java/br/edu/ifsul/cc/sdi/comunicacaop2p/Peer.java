b9e0adffdb04570d94925c3274c1356521b500dd
Gabrielle <gabriellebrambillacc@gmail.com>
2023-06-29 09:54:56 -0300

    alterações comunicação

--- src/main/java/br/edu/ifsul/cc/sdi/comunicacaop2p/Peer.java
+++ src/main/java/br/edu/ifsul/cc/sdi/comunicacaop2p/Peer.java
@@ -15,20 +15,21 @@ public class Peer extends ReceiverAdapter {
 
     private JChannel channel;
     private View view;
+    Mensagem mensagem;
 
     private HashMap<String, String> arquivosLocais;
     private HashMap<String, Address> arquivosGlobais;
 
     private void start() throws Exception {
-        
+
         arquivosLocais = new HashMap<>();
         arquivosGlobais = new HashMap<>();
 
         channel = new JChannel().setReceiver(this);
         channel.connect("Sistema_Arquivos_P2P");
-        
+
         enviaMensagem();
-        
+
         channel.close();
 
     }
@@ -41,7 +42,6 @@ public class Peer extends ReceiverAdapter {
     private void enviaMensagem() {
 
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        Mensagem mensagem;
         String entrada, nomeArquivo, conteudoArquivo;
 
         try {
@@ -49,8 +49,10 @@ public class Peer extends ReceiverAdapter {
             mensagem = new Mensagem("NOVO_PEER");
             channel.send(null, mensagem);
 
+            boolean sair = false;
+
             System.out.println("* SISTEMA DE ARQUIVOS P2P *");
-            while (true) {
+            while (!sair) {
 
                 System.out.println("\n ____________OPÇÕES____________");
                 System.out.println(" |                              |");
@@ -60,6 +62,7 @@ public class Peer extends ReceiverAdapter {
                 System.out.println(" | 4 - LISTAR_ARQUIVOS_GLOBAIS  |");
                 System.out.println(" | 5 - DELETAR_ARQUIVO          |");
                 System.out.println(" | 6 - VISUALIZAR_CONTEUDO      |");
+                System.out.println(" | 7 - SAIR                     |");
                 System.out.println(" |______________________________|\n");
 
                 System.out.println("Que ação deseja realizar?");
@@ -73,7 +76,7 @@ public class Peer extends ReceiverAdapter {
 
                         System.out.println("\nPEERS: ");
                         System.out.println(view);
-                        
+
                         break;
 
                     case "2":
@@ -88,100 +91,96 @@ public class Peer extends ReceiverAdapter {
                         System.out.println("Insira o conteúdo para o novo arquivo: ");
                         System.out.print(" > ");
                         conteudoArquivo = in.readLine().toLowerCase();
-                        
+
                         if (arquivosGlobais.get(nomeArquivo) == null) {
-                            
-                            arquivosLocais.put(nomeArquivo, conteudoArquivo);
-                            arquivosGlobais.put(nomeArquivo, this.channel.address());
-                            
-                            mensagem = new Mensagem("ARQUIVO_ADICIONADO");
-                            mensagem.setParam("nome_arquivo", nomeArquivo);
-                                    
-                            channel.send(null, mensagem);
-                            
+
+                            adicionarArquivo(nomeArquivo, conteudoArquivo);
                             System.out.println("\nArquivo adicionado com sucesso!");
-                            
-                        } else
-                            System.out.println("\nArquivo já existe! Dono: " + arquivosLocais.get(nomeArquivo));
-                        
+
+                        } else {
+                            System.out.println("\nArquivo já existe! Dono: " + arquivosGlobais.get(nomeArquivo));
+                        }
+
                         break;
 
                     case "3":
                     case "LISTAR_MEUS_ARQUIVOS":
-                        
+
                         System.out.println("\nMEUS ARQUIVOS:\n");
-                        
-                        if (!arquivosLocais.isEmpty())
+
+                        if (!arquivosLocais.isEmpty()) {
                             System.out.println(listaArquivosLocais());
-                        else
+                        } else {
                             System.out.println("Nenhum arquivo para mostrar!");
+                        }
 
                         break;
 
                     case "4":
                     case "LISTAR_ARQUIVOS_GLOBAIS":
-                        
+
                         System.out.println("\nARQUIVOS DE TODOS OS PEERS:\n");
-                        
-                        if (!arquivosGlobais.isEmpty())
+
+                        if (!arquivosGlobais.isEmpty()) {
                             System.out.println(listaArquivosGlobais());
-                        else
+                        } else {
                             System.out.println("Nenhum arquivo para mostrar!");
+                        }
 
                         break;
 
                     case "5":
                     case "DELETAR_ARQUIVO":
-                        
+
                         System.out.println("DELETAR ARQUIVO: \n");
 
                         System.out.print("Insira o nome do arquivo para deletar:");
                         System.out.print(" > ");
                         nomeArquivo = in.readLine().toLowerCase();
-                        
+
                         if (arquivosLocais.get(nomeArquivo) != null) {
-                            
-                            arquivosLocais.remove(nomeArquivo);
-                            arquivosGlobais.remove(nomeArquivo);
-                            
-                            mensagem = new Mensagem("ARQUIVO_DELETADO");
-                            mensagem.setParam("nome_arquivo", nomeArquivo);
-                                    
-                            channel.send(null, mensagem);
-                            
+
+                            deletarArquivo(nomeArquivo);
                             System.out.println("\nArquivo deletado!");
-                            
-                        } else
+
+                        } else {
                             System.out.println("\nArquivo não pode ser deletado pois não existe localmente!");
-                        
+                        }
+
                         break;
 
                     case "6":
                     case "VISUALIZAR_CONTEUDO":
-                        
+
                         System.out.println("\nVISUALIZAR CONTEÚDO DE UM ARQUIVO: \n");
 
                         System.out.print("Insira o nome do arquivo para visualizar:");
                         System.out.print(" > ");
                         nomeArquivo = in.readLine().toLowerCase();
-                        
+
                         if (arquivosLocais.get(nomeArquivo) != null) {
-                            
-                            System.out.println("\nConteúdo: "+ arquivosLocais.get(nomeArquivo));
-                            
+                            System.out.println("\nConteúdo: " + arquivosLocais.get(nomeArquivo));
                         } else {
-                            
+
                             if (arquivosGlobais.get(nomeArquivo) != null) {
-                                
-                                mensagem = new Mensagem("SOLICITA_CONTEUDO_ARQUIVO");
-                                mensagem.setParam("nome_arquivo", nomeArquivo);
-                                channel.send(null, mensagem);
-                                
-                            } else
+                                visualizaConteudoArquivo(nomeArquivo);
+                            } else {
                                 System.out.println("\nArquivo não foi encontrado em nenhum dos peers!");
-                            
+                            }
+
                         }
-                        
+
+                        break;
+
+                    case "7":
+                    case "SAIR":
+
+                        if (!arquivosLocais.isEmpty()) {
+                            saindoDeletarArquivos();
+                        }
+
+                        sair = true;
+
                         break;
 
                     default:
@@ -199,37 +198,112 @@ public class Peer extends ReceiverAdapter {
         }
 
     }
-    
+
+    public void adicionarArquivo(String nome, String conteudo) {
+
+        try {
+
+            arquivosLocais.put(nome, conteudo);
+            arquivosGlobais.put(nome, this.channel.address());
+
+            mensagem = new Mensagem("ARQUIVO_ADICIONADO");
+            mensagem.setParam("nome_arquivo", nome);
+
+            channel.send(null, mensagem);
+
+        } catch (Exception e) {
+
+            System.out.println("\nErro na comunicação com os peers! ");
+            System.out.println("ERRO: " + e);
+
+        }
+
+    }
+
     public String listaArquivosLocais() {
-        
+
         String lista = "";
-        
+
         for (String nome : arquivosLocais.keySet()) {
-            
+
             lista += "Nome: " + nome;
             lista += "\nConteúdo: " + arquivosLocais.get(nome);
             lista += "\n";
-            
+
         }
-        
+
         return lista;
-        
+
     }
-    
+
     public String listaArquivosGlobais() {
-        
+
         String lista = "";
-        
+
         for (String nome : arquivosGlobais.keySet()) {
-            
+
             lista += "Nome: " + nome;
             lista += "\nDono: " + arquivosGlobais.get(nome);
             lista += "\n";
-            
+
         }
-        
+
         return lista;
-        
+
+    }
+
+    public void deletarArquivo(String nome) throws Exception {
+
+        try {
+
+            arquivosLocais.remove(nome);
+            arquivosGlobais.remove(nome);
+
+            mensagem = new Mensagem("ARQUIVO_DELETADO");
+            mensagem.setParam("nome_arquivo", nome);
+
+            channel.send(null, mensagem);
+
+        } catch (Exception e) {
+
+            System.out.println("\nErro na comunicação com os peers! ");
+            System.out.println("ERRO: " + e);
+
+        }
+
+    }
+
+    public void visualizaConteudoArquivo(String nome) {
+
+        try {
+
+            mensagem = new Mensagem("SOLICITA_CONTEUDO_ARQUIVO");
+            mensagem.setParam("nome_arquivo", nome);
+            channel.send(null, mensagem);
+
+        } catch (Exception e) {
+
+            System.out.println("\nErro na comunicação com os peers! ");
+            System.out.println("ERRO: " + e);
+
+        }
+
+    }
+
+    public void saindoDeletarArquivos() {
+
+        try {
+
+            mensagem = new Mensagem("PEER_SAINDO");
+            channel.send(null, mensagem);
+
+        } catch (Exception e) {
+
+            System.out.println("\nErro na comunicação com os peers! ");
+            System.out.println("ERRO: " + e);
+
+        }
+
     }
 
     @Override
@@ -240,68 +314,91 @@ public class Peer extends ReceiverAdapter {
 
         try {
 
-            if (m.getSrc() != this.channel.address()) {
+            if (!m.getSrc().equals(this.channel.address())) {
 
                 switch (mensagem.getOperacao()) {
 
                     case "NOVO_PEER":
 
-                        mensagem = new Mensagem("RESPOSTA_" + mensagem.getOperacao());
-                        mensagem.setStatus(Status.OK);
+                        if (this.channel.address().equals(view.getCoord()) && !arquivosGlobais.isEmpty()) {
 
-                        String listaPeers = "";
+                            mensagem = new Mensagem("RESPOSTA_" + mensagem.getOperacao());
+                            mensagem.setStatus(Status.OK);
 
-                        for (String nome : arquivosGlobais.keySet()) {
-                            listaPeers += nome + "=" + arquivosGlobais.get(nome) + ";";
+                            String listaPeers = "";
+
+                            for (String nome : arquivosGlobais.keySet()) {
+                                listaPeers += nome + "=" + arquivosGlobais.get(nome) + ";";
+                            }
+
+                            mensagem.setParam("lista_arquivos", listaPeers);
+
+                            channel.send(m.getSrc(), mensagem);
+
                         }
 
-                        mensagem.setParam("lista_arquivos", listaPeers);
-                        
-                        channel.send(m.getSrc(), mensagem);
-                        
                         break;
 
                     case "RESPOSTA_NOVO_PEER":
-                        
+
                         String listaArquivos[] = mensagem.getParam("lista_arquivos").split(";");
-                        
+
                         for (String a : listaArquivos) {
-                            
+
                             String dadosArquivo[] = a.split("=");
                             arquivosGlobais.put(dadosArquivo[0], stringToAddress(dadosArquivo[1]));
-                            
+
                         }
-                        
+
                         break;
-                        
+
                     case "ARQUIVO_ADICIONADO":
-                        
+
                         nomeArquivo = mensagem.getParam("nome_arquivo");
                         arquivosGlobais.put(nomeArquivo, m.getSrc());
-                        
+
                         break;
-                        
+
                     case "ARQUIVO_DELETADO":
-                        
+
                         nomeArquivo = mensagem.getParam("nome_arquivo");
                         arquivosGlobais.remove(nomeArquivo);
-                        
+
                         break;
-                        
+
                     case "SOLICITA_CONTEUDO_ARQUIVO":
-                        
+
                         nomeArquivo = mensagem.getParam("nome_arquivo");
-                        
+
                         mensagem = new Mensagem("RESPOSTA_" + mensagem.getOperacao());
                         mensagem.setStatus(Status.OK);
                         mensagem.setParam("conteudo_arquivo", arquivosLocais.get(nomeArquivo));
                         channel.send(m.getSrc(), mensagem);
-                        
+
                         break;
-                        
+
                     case "RESPOSTA_SOLICITA_CONTEUDO_ARQUIVO":
-                        
+
                         System.out.println("\nConteúdo: " + mensagem.getParam("conteudo_arquivo"));
+
+                        break;
+
+                    case "PEER_SAINDO":
+
+                        HashMap<String, Address> novoArquivosGlobais = new HashMap<>();
+
+                        for (String nome : arquivosGlobais.keySet()) {
+
+                            if (!arquivosGlobais.get(nome).equals(m.getSrc())) {
+                                novoArquivosGlobais.put(nome, arquivosGlobais.get(nome));
+                            }
+
+                        }
+
+                        arquivosGlobais.clear();
+                        arquivosGlobais.putAll(novoArquivosGlobais);
+
+                        break;
 
                 }
 
@@ -315,18 +412,19 @@ public class Peer extends ReceiverAdapter {
         }
 
     }
-    
+
     public Address stringToAddress(String stringAddress) {
-        
+
         for (Address a : view.getMembersRaw()) {
-            
-            if (stringAddress.equals(a.toString()))
+
+            if (stringAddress.equals(a.toString())) {
                 return a;
-            
+            }
+
         }
-        
+
         return null;
-        
+
     }
 
     public static void main(String[] args) throws Exception {
